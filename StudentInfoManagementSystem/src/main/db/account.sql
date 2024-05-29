5c81463efde8b88e4bc61f190373df412a166473
Zhou Ziyu <ziyu_zhou2002@mail.ustc.edu.cn>
2024-05-29 10:39:49 +0800

    Create account tables.

---
+++ StudentInfoManagementSystem/src/main/db/account.sql
@@ -0,0 +1,37 @@
+-- Create table for student account
+CREATE TABLE StudentAccount(
+    StudentUsername VARCHAR(15) PRIMARY KEY,                -- Username, same as student ID
+    StudentPassword VARCHAR(255) DEFAULT '123456' NOT NULL, -- Password, default is 123456
+    -- Define the constraints here
+    -- StudentUsername is a foreign key to Student
+    FOREIGN KEY (StudentUsername) REFERENCES Enrolment(StudentID),
+    -- StudentPassword is at least 6 characters long
+    CHECK (
+        LENGTH(StudentPassword) >= 6
+    )
+);
+
+-- Create table for teacher account
+CREATE TABLE TeacherAccount(
+    TeacherUsername VARCHAR(15) PRIMARY KEY,                        -- Username, here only administrator
+    TeacherPassword VARCHAR(255) DEFAULT 'administrator' NOT NULL,  -- Password, default is administrator
+    -- Define the constraints here
+    -- TeacherPassword is at least 6 characters long
+    CHECK (
+        LENGTH(TeacherPassword) >= 6
+    )
+);
+
+INSERT INTO
+    TeacherAccount(TeacherUsername, TeacherPassword)
+VALUES ('administrator', 'administrator');
+
+-- Trigger to automatically create student account
+CREATE TRIGGER CreateStudentAccount AFTER INSERT ON Enrolment
+FOR EACH ROW
+BEGIN
+   --  New StudentAccount: NEW.StudentID, 123456
+    INSERT INTO
+        StudentAccount(StudentUsername)
+    VALUES (NEW.StudentID);
+END;
