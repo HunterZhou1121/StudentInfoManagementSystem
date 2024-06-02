ae9fa297203a9de6efe92611e34f199031315376
Zhou Ziyu <ziyu_zhou2002@mail.ustc.edu.cn>
2024-06-02 12:40:54 +0800

    Use another image manager for imageUploader.

--- StudentInfoManagementSystem/src/main/java/cn/edu/ustc/studentinfomanagementsystem/utils/imageUploader.java
+++ StudentInfoManagementSystem/src/main/java/cn/edu/ustc/studentinfomanagementsystem/utils/imageUploader.java
@@ -1,19 +1,11 @@
 package cn.edu.ustc.studentinfomanagementsystem.utils;
 import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.client.methods.RequestBuilder;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
-import org.apache.http.entity.mime.content.FileBody;
-import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -29,67 +21,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class imageUploader {
-    private static final String TOKEN_URL = "https://www.imgtp.com/api/token";
-    private static final String UPLOAD_URL = "https://www.imgtp.com/api/upload";
-    private String AUTHORIZATION;
+    private static final String UPLOAD_URL = "https://www.imgurl.org/api/v2/upload";
+    private static final String AUTHORIZATION = "b9618b3a87522cec7a1ed000334e7186";
 
-    imageUploader(String email, String password) {
-        // get api token
+    private static final String UID = "081a11e22460efe0cebd44e8e6972137";
+
+    public static String uploadImage(File file) {
         try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-            HttpPost httpPost = new HttpPost(TOKEN_URL);
-            List<NameValuePair> formParams = new ArrayList<>();
-            formParams.add(new BasicNameValuePair("email", email));
-            formParams.add(new BasicNameValuePair("password", password));
-            UrlEncodedFormEntity uefEntity = new UrlEncodedFormEntity(formParams, "UTF-8");
-            httpPost.setEntity(uefEntity);
-            try (CloseableHttpResponse response = httpClient.execute(httpPost)) {
-                int statusCode = response.getStatusLine().getStatusCode();
-                if (statusCode >= 200 && statusCode < 300) {
-                    HttpEntity responseEntity = response.getEntity();
-                    String responseString = EntityUtils.toString(responseEntity, StandardCharsets.UTF_8);
-                    // extract code and token from the response string
-                    // {"code":200,"msg":"success","data":{"token":"fbd42b37dc03fc18717890fea3160d1d"},"time":1717051958}
-                    JSONObject jsonObject = new JSONObject(responseString);
-                    int code = jsonObject.getInt("code");
-                    if (code == 200) {
-                        JSONObject dataObj = jsonObject.getJSONObject("data");
-                        AUTHORIZATION = dataObj.getString("token");
-                    } else {
-                        System.err.println("Get token failed. Code: " + code);
-                    }
-                } else {
-                    System.err.println("Get token failed. Status code: " + statusCode);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    imageUploader() {
-        AUTHORIZATION = "fbd42b37dc03fc18717890fea3160d1d";
-    }
-
-    public String uploadImage(File file) {
-        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-//            HttpPost httpPost = new HttpPost(UPLOAD_URL);
-            // specify token
-            StringBody token = new StringBody(AUTHORIZATION, ContentType.TEXT_PLAIN);
             // upload file
+            HttpPost httpPost = new HttpPost(UPLOAD_URL);
             HttpEntity entity = MultipartEntityBuilder.create()
-                    .addBinaryBody("image", file)
+                    .addBinaryBody("file", file)
+                    .addTextBody("uid", UID, ContentType.TEXT_PLAIN)
                     .addTextBody("token", AUTHORIZATION, ContentType.TEXT_PLAIN)
                     .build();
-            HttpUriRequest postRequest = RequestBuilder
-                    .post(UPLOAD_URL)
-                    .setEntity(entity)
-                    .build();
-            System.out.println("Executing request " + postRequest.getRequestLine());
-//            httpPost.setEntity(entity);
+            httpPost.setEntity(entity);
+            System.out.println("Executing request " + httpPost.getRequestLine());
             // send the request and get the response
-            try (CloseableHttpResponse response = httpClient.execute(postRequest)) {
+            try (CloseableHttpResponse response = httpClient.execute(httpPost)) {
                 int statusCode = response.getStatusLine().getStatusCode();
                 // 200: OK
                 if (statusCode >= 200 && statusCode < 300) {
@@ -115,8 +64,7 @@ public class imageUploader {
         }
         return "";
     }
-
-    public void uploadAllLocalImages() {
+    public static void uploadAllLocalImages() {
         // find all students in the database whose "PhotoURL" is null
         String sql = "SELECT Enrolment.StudentID FROM Enrolment, Student WHERE Enrolment.ID = Student.ID AND Student.PhotoURL IS NULL";
         // test connection
@@ -130,22 +78,14 @@ public class imageUploader {
                 // the picture is in "src/main/resources/cn/edu/ustc/studentinfomanagementsystem/pics/"
                 String rootPath = "src/main/resources/cn/edu/ustc/studentinfomanagementsystem/pics/";
                 String oldPicPath = rootPath + StudentID + ".jpg";
-                String newPicPath = rootPath + StudentID + "/" + StudentID + ".jpg";
-                // move them to their respective folders
-                try {
-                    fileManager.createDirectory(rootPath + StudentID + "/");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    fileManager.renameFile(oldPicPath, newPicPath);
-                } catch (IOException e) {
-                    e.printStackTrace();
+                File file = new File(oldPicPath);
+                // continue if the file does not exist
+                if (!file.exists()) {
+                    continue;
                 }
                 // upload the picture
-                File file = new File(newPicPath);
-                imageUploader uploader = new imageUploader("z1057247521@hotmail.com", "123456");
-                String url = uploader.uploadImage(file);
+                String url = uploadImage(file);
+                System.out.println("StudentID " + StudentID + " PhotoURL: " + url);
                 // query the student's ID from Enrolment, using StudentID
                 String querySQL = "SELECT ID FROM Enrolment WHERE StudentID = ?";
                 String ID = "";
@@ -157,7 +97,7 @@ public class imageUploader {
                         }
                     }
                 } catch (SQLException e) {
-                    e.printStackTrace();
+                    DBConnection.SQLExceptionHandler(e);
                 }
                 // update the database
                 String updateSQL = "UPDATE Student SET PhotoURL = ? WHERE ID = ?";
@@ -166,49 +106,21 @@ public class imageUploader {
                     updatePS.setString(2, ID);
                     updatePS.executeUpdate();
                 } catch (SQLException e) {
-                    e.printStackTrace();
+                    DBConnection.SQLExceptionHandler(e);
+                }
+                // delete the local picture
+                if (file.delete()) {
+                    System.out.println("Deleted the file: " + file.getName());
+                } else {
+                    System.out.println("Failed to delete the file.");
                 }
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            DBConnection.SQLExceptionHandler(e);
         }
     }
     public static void main(String[] args) {
-//        // path to a picture
-//        String oldFilePath = "src/main/resources/cn/edu/ustc/studentinfomanagementsystem/pics/Wolffe.jpg";
-//        // copy the picture
-//        try {
-//            fileManager.copyFile(oldFilePath, "src/main/resources/cn/edu/ustc/studentinfomanagementsystem/pics/Wolffe.jpg1");
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        // create a directory for the student
-//        String dirPath = "src/main/resources/cn/edu/ustc/studentinfomanagementsystem/pics/";
-//        String studentID = "PB21111738";
-//        try {
-//            fileManager.createDirectory(dirPath + studentID);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        // move and rename the picture
-//        String newFilePath = dirPath + studentID + "/" + studentID + ".jpg";
-//        try {
-//            fileManager.renameFile(oldFilePath, newFilePath);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        // upload the picture
-//        File file = new File(newFilePath);
-//        imageUploader uploader = new imageUploader("z1057247521@hotmail.com", "123456");
-//        System.out.println(uploader.uploadImage(file));
-//        // rename the copy
-//        try {
-//            fileManager.renameFile("src/main/resources/cn/edu/ustc/studentinfomanagementsystem/pics/Wolffe.jpg1", oldFilePath);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-        imageUploader uploader = new imageUploader("z1057247521@hotmail.com", "123456");
-        uploader.uploadAllLocalImages();
+        uploadAllLocalImages();
     }
 
 }
